2e86ae1acb44e67d52926f91cba6c235ba4a2299
Duarte Manuel Bento Dias <duartedias@student.dei.uc.pt>
2021-05-22 19:12:32 +0100

    Added B Final Version(Somehow forgot to update)

--- B/main.cpp
+++ B/main.cpp
@@ -1,10 +1,12 @@
 // Duarte Dias 2018293526
 // Maria Dias 2018274188
 #include <iostream>
+#include <cmath>
 
 #define MODULO 1000000007
 using namespace std;
-int tt, n, h, H;
+long cached = 0;
+int tt, n, h, H, hidx;
 int dpu[60001][501];
 int dpd[60001][501];
 
@@ -12,51 +14,44 @@ int mod_abs(int a, int mod) { return ((a % mod) + mod) % mod; }
 int mod_add(int a, int b, int mod) { return (mod_abs(a, mod) + mod_abs(b, mod)) % mod; }
 int mod_sub(int a, int b, int mod) { return mod_add(a, -b, mod); }
 
-int solver(int ch, int k, bool up) {
-    if (ch > H || ch < h || k >= n)  // Max Height, Min Height conditions, Index starts at 0, therefore n -1
-        return 0;
-    if (ch == h && k != 0)  // Ground level
-        return 1;
-    if (up) {
-        if (dpu[ch][k] != -1) {
-            return dpu[ch][k];
-        }
-    } else {
-        if (dpd[ch][k] != -1) {
-            return dpd[ch][k];
-        }
-    }
-
-    int res = 0;
-
-    // Go Up
-    if (up) {
-        for (int i = 1; i < h; i++) {
-            if ((ch + i) - ((h - 1) * ((n - 1) - (k + 1))) <= h) res = mod_add(res, solver(ch + i, k + 1, true), MODULO);
-            // res += solver(ch + i, k + 1, true);
-        }
-    }
-
-    // Go Down
-    for (int i = 1; i < h; i++) {
-        res = mod_add(res, solver(ch - i, k + 1, false), MODULO);
-        // res += solver(ch - i, k + 1, false);
-    }
-    /* cout << res << std::endl;
-    cout << "----" << endl; */
-    if (up) {
-        dpu[ch][k] = res;
-    } else {
-        dpd[ch][k] = res;
-    }
-
-    return res;
-    // return res;
-}
-
 void solve() {
-    int res = solver(h, 0, true);
-    cout << res << endl;
+    int sol = 0, sum, i, j;
+    dpu[hidx][0] = 1;
+    for (i = 0; i < n - 1; i++) {
+        sum = 0;
+        for (j = hidx + i; j < H - 1; j++) {
+            sum = mod_add(sum, dpu[j][i], MODULO);
+            dpu[j + 1][i + 1] = sum;
+            sum = mod_sub(sum, dpu[j - (h - 2)][i], MODULO);  // -1 because of using h-1 and an extra -1 because of index adjusting
+            if (sum == 0) {
+                break;
+            }
+        }
+
+        sum = 0;
+        for (; j > h - 1; j--) {
+            sum = mod_add(sum, dpu[j][i], MODULO);
+            dpd[j - 1][i + 1] = mod_add(dpd[j - 1][i + 1], sum, MODULO);
+            if (j + (h - 2) < H )
+                sum = mod_sub(sum, dpu[j + (h - 2)][i], MODULO);  // -1 because of using h-1 and an extra -1 because of index adjusting
+        }
+
+        sum = 0;
+        for (int j = min(i * (h - 1) - 1, H); j > h - 1; j--) {
+            sum = mod_add(sum, dpd[j][i], MODULO);
+            dpd[j - 1][i + 1] = mod_add(dpd[j - 1][i + 1], sum, MODULO);
+            if (j + (h - 2) < H )
+                sum = mod_sub(sum, dpd[j + (h - 2)][i], MODULO);  // -1 because of using h-1 and an extra -1 because of index adjusting
+        }
+
+    }
+
+
+    for (int i = 0; i < n; i++) {
+        sol = mod_add(sol, dpd[hidx][i], MODULO);
+    }
+    cout << sol << endl;
+
 }
 
 int main() {
@@ -65,8 +60,9 @@ int main() {
     cin >> tt;
     while (tt--) {
         cin >> n >> h >> H;
-        std::fill(dpu[0], dpu[H] + n, -1);
-        std::fill(dpd[0], dpd[H] + n, -1);
+        hidx = h - 1;
+        std::fill(dpu[0], dpu[H] + n, 0);
+        std::fill(dpd[0], dpd[H] + n, 0);
         solve();
     }
 }
